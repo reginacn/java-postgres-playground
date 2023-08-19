5ae85fc134ea0fa5fb4324fd4c4dab3a997428ac
Regina Zanette <reginacn@gmail.com>
2023-08-19 20:21:07 +0000

    testando tipos de dados *

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -4,5 +4,13 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World: " + Math.random());
+
+        System.out.println("Hello testando tipos núméricos");
+        
+        int idade = 10;        
+        System.out.println("a idade é: " + idade);
+        double peso = 80.5;
+        System.out.println("o peso é: " + peso);
+
     }
 }
