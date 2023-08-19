7e338aa70efc6461be6884ae9d8af13eb7e24c0c
Regina Zanette <reginacn@gmail.com>
2023-08-19 20:44:17 +0000

    teste de tipos de dados

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -12,5 +12,16 @@ public class HelloWorld {
         double peso = 80.5;
         System.out.println("o peso é: " + peso);
 
+        int idadeInt = 85;
+        //cast = conversão de tipo;
+        byte idadeByteConvertida = (byte)idadeInt; //só converter se eu sei que realmente cabe pelo tamanho do tipo. Senão pode dar estouro
+        System.out.println("* idadeInt: " + idadeInt + " * idadeByteConvertida: " + idadeByteConvertida);
+
+        char sexo = 'F';
+        System.out.println("sexo: " + sexo);      
+          
+        boolean necessidadesEspeciais = false;
+        System.out.println("tem necessidades especiais: " + necessidadesEspeciais);
+
     }
 }
